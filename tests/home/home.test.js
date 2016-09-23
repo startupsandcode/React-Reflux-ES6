af744badddf931290d3ec426a44944fb7741753a
lotekmedia <me@johnmann.org>
2016-09-23 16:40:22 -0400

    Updated Test and Fixed History version

--- tests/home/home.test.js
+++ tests/home/home.test.js
@@ -9,17 +9,19 @@ function setup() {
     const props = {
         sharedStore: Immutable.Map({
             title: 'HomePage Stuff'
-        }),
-        homeStore: Immutable.Map({
-            firstName: 'John'
         })
     };
-    return shallow(<HomeForm {...props} />);
+    let wrapper = shallow(<HomeForm {...props} />);
+    wrapper.setState({ homeStore: Immutable.Map({ firstName: 'John' }) });
+    return wrapper;
 }
 describe('HomePage via Enzyme', () => {
     it('render page and h1', () => {
         const wrapper = setup();
         expect(wrapper.find('h1').length).toBe(1);
         expect(wrapper.find('h1').contains('HomePage Stuff')).toBe(true);
+        expect(wrapper.find('input').length).toBe(2);
+        expect(wrapper.find('input').get(0).props.value).toBe('John');
+
     });
 });
