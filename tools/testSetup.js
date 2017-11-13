9aa5cdbccb1b987423d197a1865909c0ebb8878b
John Mann <me@johnmann.org>
2017-11-13 14:50:28 -0500

    Updated for new jsdom version

--- tools/testSetup.js
+++ tools/testSetup.js
@@ -6,11 +6,14 @@ require('babel-register')();
 
 // Configure JSDOM and set global variables
 // to simulate a browser environment for tests.
-var jsdom = require('jsdom').jsdom;
+var jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+const { document } = (new JSDOM('')).window;
 
 var exposedProperties = ['window', 'navigator', 'document'];
 
-global.document = jsdom('');
+global.document = document;
 global.window = document.defaultView;
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
